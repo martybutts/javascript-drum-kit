24fc563afa91e92d9b76ad85bb65996778358b5b
Martin <martinbutts@clear.net.nz>
2017-02-03 11:17:38 +1300

    everything up to addEventListener for key transition

--- public/index.js
+++ public/index.js
@@ -1,3 +1,10 @@
 window.addEventListener('keydown', function(e) {
-  console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) //using attribute selector as in css []
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return; //stop function from running all together
+  audio.currentTime = 0
+  audio.play()
+  key.classList.add('playing') //can also use classList.remove, classList.toggle
 })
+
+const keys = document.querySelectorAll('.key')
